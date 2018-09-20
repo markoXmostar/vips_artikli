15a4404cf03bec871f1993036003df0dd024087f
Matej Miljko <jasenicar@gmail.com>
2018-09-20 18:45:33 +0200

    Merge branch 'master' of https://github.com/markoXmostar/vips_artikli into markoXmostar-master
    # Conflicts:
    #	.idea/misc.xml
    Resolved

--- app/src/main/java/com/example/marko/vips_artikli/glavne_aktivnosti/App1UnosStavkeActivity.java
+++ app/src/main/java/com/example/marko/vips_artikli/glavne_aktivnosti/App1UnosStavkeActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
