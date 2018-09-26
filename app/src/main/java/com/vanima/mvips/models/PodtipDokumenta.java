bfd0178aac43f4c7b172349d9a2e0326f640788d
Fr0ssT <jaenicar@gmail.com>
2018-09-26 15:52:02 +0200

    Sve refaktorirano na com.vanima.mvips

---
+++ app/src/main/java/com/vanima/mvips/models/PodtipDokumenta.java
@@ -0,0 +1,46 @@
+package com.vanima.mvips.models;
+
+/**
+ * Created by rac157 on 14.12.2017.
+ */
+
+public class PodtipDokumenta {
+    private long id;
+    private String naziv;
+    private long rid;
+
+    public PodtipDokumenta(long id, String naziv, long rid) {
+        this.id = id;
+        this.naziv = naziv;
+        this.rid = rid;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public long getRid() {
+        return rid;
+    }
+
+    public void setRid(long rid) {
+        this.rid = rid;
+    }
+
+    @Override
+    public String toString() {
+        return naziv;
+    }
+}
---
+++ app/src/main/java/com/vanima/mvips/utils/ClientIF.java
@@ -0,0 +1,10 @@
+package com.vanima.mvips.utils;
+
+/**
+ * Created by marko on 1.11.2017.
+ */
+public interface ClientIF {
+
+    void onResponseReceived(int result);
+
+}
