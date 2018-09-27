43f8616d632396d6efb47ea6bd5a9fcc25fb997b
Matej Miljko <matej.miljko@jasenica.com>
2018-09-27 13:11:26 +0200

    Privremeno uklonjen fingerprint sa aplikacije Idalje ne radi na api<20

--- app/src/main/java/com/vanima/mvips/activities/PinActivity.java
+++ app/src/main/java/com/vanima/mvips/activities/PinActivity.java
@@ -1,18 +1,8 @@
 package com.vanima.mvips.activities;
 
-import android.Manifest;
 import android.app.Activity;
-import android.app.KeyguardManager;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.fingerprint.FingerprintManager;
-import android.os.Build;
 import android.os.Handler;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyPermanentlyInvalidatedException;
-import android.security.keystore.KeyProperties;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,43 +12,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.vanima.mvips.utils.FingerprintHandler;
 import com.vanima.mvips.R;
 import com.vanima.mvips.utils.postavkeAplikacije;
 
-import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-
 public class PinActivity extends AppCompatActivity {
+
     boolean doubleBackToExitPressedOnce = false;
     int brojPokusaja = 1;
     private String TAG = "PIN_ACTIVITY";
     EditText txtPin;
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDellAll, btnDell1;
     static String requiredPin;
-    private static final String KEY_NAME = "fingerprintKeyName";
-    private Cipher cipher;
-    private KeyStore keyStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin);
+
         requiredPin = new postavkeAplikacije(PinActivity.this).getPin();
         txtPin = findViewById(R.id.txtPass_pin);
+
         postaviDugmice();
+
         Button btnOK = findViewById(R.id.btnOK_pin);
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,55 +64,13 @@ public class PinActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
     protected void onResume(){
         super.onResume();
-        Button btnOK = findViewById(R.id.btnOK_pin);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //Get an instance of KeyguardManager and FingerprintManager//
-            KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-            FingerprintManager fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
-
-            //Check whether the device has a fingerprint sensor//
-            if (!fingerprintManager.isHardwareDetected()) {
-                // If a fingerprint sensor isn’t available, then inform the user that they’ll be unable to use your app’s fingerprint functionality//
-                txtPin.setCompoundDrawables(null,null,null,null);
-            }
-            //Check whether the user has granted your app the USE_FINGERPRINT permission//
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
-                // If your app doesn't have this permission, then display the following text//
-                txtPin.setCompoundDrawables(null,null,null,null);
-            }
-
-            //Check that the user has registered at least one fingerprint//
-            if (!fingerprintManager.hasEnrolledFingerprints()) {
-                // If the user hasn’t configured any fingerprints, then display the following message//
-                txtPin.setCompoundDrawables(null,null,null,null);
-            }
-
-            //Check that the lockscreen is secured//
-            if (!keyguardManager.isKeyguardSecure()) {
-                // If the user hasn’t secured their lockscreen with a PIN password or pattern, then display the following text//
-                txtPin.setCompoundDrawables(null,null,null,null);
-            } else {
-                try {
-                    generateKey();
-                } catch (FingerprintException e) {
-                    e.printStackTrace();
-                }
-
-                if (initCipher()) {
-                    //If the cipher is initialized successfully, then create a CryptoObject instance//
-                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
-
-                    // Here, I’m referencing the FingerprintHandler class that we’ll create in the next section. This class will be responsible
-                    // for starting the authentication process (via the startAuth method) and processing the authentication process events//
-                    FingerprintHandler helper = new FingerprintHandler(this);
-                    helper.startAuth(fingerprintManager, cryptoObject, txtPin, btnOK, requiredPin);
-
-                }
-            }
-        }
     }
 
     private void postaviDugmice() {
@@ -232,84 +165,6 @@ public class PinActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void generateKey() throws FingerprintException {
-        try {
-            // Obtain a reference to the Keystore using the standard Android keystore container identifier (“AndroidKeystore”)//
-            keyStore = KeyStore.getInstance("AndroidKeyStore");
-
-            //Generate the key//
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
-
-            //Initialize an empty KeyStore//
-            keyStore.load(null);
-
-            //Initialize the KeyGenerator//
-            keyGenerator.init(new
-
-                    //Specify the operation(s) this key can be used for//
-                    KeyGenParameterSpec.Builder(KEY_NAME,
-                    KeyProperties.PURPOSE_ENCRYPT |
-                            KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-
-                    //Configure this key so that the user has to confirm their identity with a fingerprint each time they want to use it//
-                    .setUserAuthenticationRequired(true)
-                    .setEncryptionPaddings(
-                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
-                    .build());
-
-            //Generate the key//
-            keyGenerator.generateKey();
-
-        } catch (KeyStoreException
-                | NoSuchAlgorithmException
-                | NoSuchProviderException
-                | InvalidAlgorithmParameterException
-                | CertificateException
-                | IOException exc) {
-            exc.printStackTrace();
-            throw new FingerprintException(exc);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public boolean initCipher() {
-        try {
-            //Obtain a cipher instance and configure it with the properties required for fingerprint authentication//
-            cipher = Cipher.getInstance(
-                    KeyProperties.KEY_ALGORITHM_AES + "/"
-                            + KeyProperties.BLOCK_MODE_CBC + "/"
-                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-        } catch (NoSuchAlgorithmException |
-                NoSuchPaddingException e) {
-            throw new RuntimeException("Failed to get Cipher", e);
-        }
-
-        try {
-            keyStore.load(null);
-            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
-                    null);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            //Return true if the cipher has been initialized successfully//
-            return true;
-        } catch (KeyPermanentlyInvalidatedException e) {
-
-            //Return false if cipher initialization failed//
-            return false;
-        } catch (KeyStoreException | CertificateException
-                | UnrecoverableKeyException | IOException
-                | NoSuchAlgorithmException | InvalidKeyException e) {
-            throw new RuntimeException("Failed to init Cipher", e);
-        }
-    }
-
-    private class FingerprintException extends Exception {
-        private FingerprintException(Exception e) {
-            super(e);
-        }
     }
 
     // čeka na dvostruki klik nazad da ne bi bilo slučajnih izlaza iz aplikacije
